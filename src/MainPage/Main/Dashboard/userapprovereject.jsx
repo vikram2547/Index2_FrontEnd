e87eee448fbc346a98270cddf9fbb3b8e370db48
unknown <mvikram104@gmail.com>
2024-11-26 15:03:59 +0530

    done

--- src/MainPage/Main/Dashboard/userapprovereject.jsx
+++ src/MainPage/Main/Dashboard/userapprovereject.jsx
@@ -1,137 +1,4 @@
-// import React, { useState, useEffect } from "react";
-// import { Worker, Viewer } from "@react-pdf-viewer/core";
-// import "@react-pdf-viewer/core/lib/styles/index.css"; // Required styles
-// import { pdfjs } from "react-pdf";
-// import { API_HOST } from "../../../base_URL/http";
-// import { Link, useHistory } from "react-router-dom";
-// import { useDispatch, useSelector } from "react-redux";
-// import { message } from "antd";
-// import { ResetApproveState, UserApprove } from "../../../store/userapprove";
-// import { ResetRejectedState, UserReject } from "../../../store/userreject";
-
-
-// function UserApproveReject() {
-//   const [pdfSrc, setPdfSrc] = useState(""); // Store PDF file URL
-//   const dispatch = useDispatch();
-//   const history = useHistory();
-//   const token = useSelector((state) => state.login.token); // Retrieve token from Redux store
-//   const approveResponse = useSelector((state) => state.userapprove.userapprove); // Get response from Approve action
-//   const rejectResponse = useSelector((state) => state.userreject.userreject); // Get response from Reject action
-
-//   useEffect(() => {
-//     const storedpdfFileId = localStorage.getItem("UserPdfFileId");
-//     if (storedpdfFileId) {
-//       const fullFileUrl = `${API_HOST}scan/serve_tiff_as_pdf/${storedpdfFileId}/`;
-//       setPdfSrc(fullFileUrl); // Set the PDF URL
-//     }
-//   }, []);
-
-//   const dispatchApproveAction = () => {
-//     const storedpdfFileId = localStorage.getItem("UserPdfFileId");
-//     if (storedpdfFileId) {
-//       dispatch(UserApprove(token, storedpdfFileId)); // Dispatch the Approve action
-//     }
-//   };
-
-//   const dispatchRejectAction = () => {
-//     const storedpdfFileId = localStorage.getItem("UserPdfFileId");
-//     if (storedpdfFileId) {
-//       dispatch(UserReject(token, storedpdfFileId)); // Dispatch the Reject action
-//     }
-//   };
-
-//   useEffect(() => {
-//     if (approveResponse?.message) {
-//         message.success("File Approved.", 3);
-//         dispatch(ResetApproveState());
-//       history.push("/app/main/userqcchecked-files"); // Navigate to processed files
-//     }
-//   }, [approveResponse]);
-
-//   useEffect(() => {
-//     if (rejectResponse?.message) {
-//         message.success("File Rejected.", 3);
-//         dispatch(ResetRejectedState());
-//       history.push("/app/main/userqcchecked-files"); // Navigate to processed files
-//     }
-//   }, [rejectResponse]);
-
-//   useEffect(() => {
-//     dispatch(ResetApproveState());
-//     dispatch(ResetRejectedState());
-// }, [dispatch]);
-
-//   return (
-//     <div>
-//       <div className="page-wrapper">
-//         <div className="content container-fluid">
-//           {/* Page Header */}
-//           <div className="page-header">
-//             <div className="row align-items-center">
-//               <div className="col">
-//                 <h3 className="page-title">Files</h3>
-//                 <ul className="breadcrumb">
-//                   <li className="breadcrumb-item">
-//                     <Link to="/app/main/dashboard">Dashboard</Link>
-//                   </li>
-//                   <li className="breadcrumb-item active">Tiff Files As Pdf</li>
-//                 </ul>
-//               </div>
-//             </div>
-//           </div>
-
-//           <div className="row align-items-center mb-4">
-//             <div className="col-md-2">
-//               <button
-//                 className="btn btn-success btn-block"
-//                 onClick={dispatchApproveAction} // Call the function that dispatches the approve action
-//               >
-//                 Approve
-//               </button>
-//             </div>
-
-//             <div className="col-md-2">
-//               <button
-//                 className="btn btn-danger btn-block"
-//                 onClick={dispatchRejectAction} // Call the function that dispatches the reject action
-//               >
-//                 Reject
-//               </button>
-//             </div>
-//           </div>
-//           {/* /Page Header */}
-
-//           {/* PDF Viewer with adjustments */}
-//           {pdfSrc && (
-//             <div
-//               style={{
-//                 maxWidth: "100%",
-//                 maxHeight: "50vh", // Limit the height to 50% of the viewport height
-//               }}
-//             >
-//               <Worker
-//                 workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}
-//               >
-//                 <Viewer
-//                   fileUrl={pdfSrc}
-//                   defaultScale={1.0} // Set a default scale to make the PDF visible
-//                   scale={1.0} // You can tweak this scale value for different zoom levels
-//                 />
-//               </Worker>
-//             </div>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default UserApproveReject;
-
 import React, { useState, useEffect } from "react";
-import { Worker, Viewer } from "@react-pdf-viewer/core";
-import "@react-pdf-viewer/core/lib/styles/index.css"; // Required styles
-import { pdfjs } from "react-pdf";
 import { API_HOST } from "../../../base_URL/http";
 import { Link, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -150,17 +17,17 @@ function UserApproveReject() {
   const rejectResponse = useSelector((state) => state.userreject.userreject); // Get response from Reject action
 
   useEffect(() => {
-    const storedpdfFileId = localStorage.getItem("UserPdfFileId");
-    if (storedpdfFileId) {
-      const fullFileUrl = `${API_HOST}scan/serve_tiff_as_pdf/${storedpdfFileId}/`;
+    const storedPdfFileId = localStorage.getItem("UserPdfFileId");
+    if (storedPdfFileId) {
+      const fullFileUrl = `${API_HOST}scan/serve_tiff_as_pdf/${storedPdfFileId}/`;
       setPdfSrc(fullFileUrl); // Set the PDF URL
     }
   }, []);
 
   const dispatchApproveAction = () => {
-    const storedpdfFileId = localStorage.getItem("UserPdfFileId");
-    if (storedpdfFileId) {
-      dispatch(UserApprove(token, storedpdfFileId)); // Dispatch the Approve action
+    const storedPdfFileId = localStorage.getItem("UserPdfFileId");
+    if (storedPdfFileId) {
+      dispatch(UserApprove(token, storedPdfFileId)); // Dispatch the Approve action
     }
   };
 
@@ -174,11 +41,9 @@ function UserApproveReject() {
       return;
     }
 
-    const storedpdfFileId = localStorage.getItem("UserPdfFileId");
-    if (storedpdfFileId) {
-      if (storedpdfFileId) {
-        dispatch(UserReject(token, storedpdfFileId, { remark })); 
-    }     
+    const storedPdfFileId = localStorage.getItem("UserPdfFileId");
+    if (storedPdfFileId) {
+      dispatch(UserReject(token, storedPdfFileId, { remark }));
     }
 
     setIsRejectModalVisible(false); // Close the modal
@@ -224,7 +89,7 @@ function UserApproveReject() {
                   <li className="breadcrumb-item">
                     <Link to="/app/main/dashboard">Dashboard</Link>
                   </li>
-                  <li className="breadcrumb-item active">Tiff Files As Pdf</li>
+                  <li className="breadcrumb-item active">Tiff Files As PDF</li>
                 </ul>
               </div>
             </div>
@@ -251,24 +116,27 @@ function UserApproveReject() {
           </div>
           {/* /Page Header */}
 
-          {/* PDF Viewer with adjustments */}
-          {pdfSrc && (
+          {/* PDF Viewer */}
+          {pdfSrc ? (
             <div
               style={{
-                maxWidth: "100%",
-                maxHeight: "50vh", // Limit the height to 50% of the viewport height
+                width: "100%",
+                height: "80vh", // Adjust viewer height
+                border: "1px solid #ddd",
               }}
             >
-              <Worker
-                workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}
-              >
-                <Viewer
-                  fileUrl={pdfSrc}
-                  defaultScale={1.0} // Set a default scale to make the PDF visible
-                  scale={1.0} // You can tweak this scale value for different zoom levels
-                />
-              </Worker>
+              <iframe
+                src={pdfSrc}
+                title="PDF Viewer"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  border: "none",
+                }}
+              ></iframe>
             </div>
+          ) : (
+            <p>No PDF file available</p>
           )}
         </div>
       </div>
